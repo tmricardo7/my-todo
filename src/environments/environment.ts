0236fda433254b0cb0e383d10f93958c7623705d
Ricardo Torres <tmricardo7@gmail.com>
2023-01-16 21:01:30 -0600

    add pwa progress

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'my-todo-a4196',
+    appId: '1:1017737728994:web:b12355881623d332439fbb',
+    databaseURL: 'https://my-todo-a4196-default-rtdb.firebaseio.com',
+    storageBucket: 'my-todo-a4196.appspot.com',
+    apiKey: 'AIzaSyCwBqbj331Z9mf2810fywkHDHIJjRJ5oIM',
+    authDomain: 'my-todo-a4196.firebaseapp.com',
+    messagingSenderId: '1017737728994',
+    measurementId: 'G-8NFFNCNL2D',
+  }
+  };
